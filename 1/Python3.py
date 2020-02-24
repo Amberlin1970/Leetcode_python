d73a0048c548a75e9e71189d9591a9c24e71e5f5
Amberlin1970 <37952468+Amberlin1970@users.noreply.github.com>
2020-02-24 20:50:36 +0800

    Create Python3.py

---
+++ 1/Python3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sub,a = 0, 0
+        fi = []
+        for i in range(len(nums)):
+            sub = target - nums[i]
+            if sub in nums:
+                a = nums.index(sub)
+                if a!= i:
+                    fi.extend([i,a])
+                    return fi              
+                else:continue                   
+            else:
+                continue
+自己感觉的考点：列表
